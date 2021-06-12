09c0b0de3bcd535314dedecb00743eda12a67184
hexmaster111 <Hexmaster111@gmail.com>
2021-06-12 10:04:36 -0500

    We now have a servo to release

--- arduino-code/Arduino-Key-Release/src/main.cpp
+++ arduino-code/Arduino-Key-Release/src/main.cpp
@@ -1,11 +1,16 @@
 #include <Arduino.h>
+#include <Servo.h>
 
 int datafromUser = 0;
+
+Servo keyReleaseServo;
+
 void setup()
 {
   // put your setup code here, to run once:
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  keyReleaseServo.attach(2);
 }
 
 void loop()
@@ -15,13 +20,16 @@ void loop()
   {
     datafromUser = Serial.read();
   }
-
+ //ima make this a case statement but for now this is good enough for testing
+ //@TODO Make this a case statement
   if (datafromUser == '1')
   {
     digitalWrite(LED_BUILTIN, HIGH);
+    keyReleaseServo.write(0);
   }
   else if (datafromUser == '0')
   {
     digitalWrite(LED_BUILTIN, LOW);
+    keyReleaseServo.write(180);
   }
 }
